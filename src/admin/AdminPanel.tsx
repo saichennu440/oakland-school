07801358cd4874d869ec061f5f1ad7e3b5b456ca
mesaichennu <chennusaiprasad24@gmail.com>
2025-12-04 20:42:07 +0530

    removed id from admin panel

--- src/admin/AdminPanel.tsx
+++ src/admin/AdminPanel.tsx
@@ -140,7 +140,7 @@ export default function AdminPanel() {
           <button
             onClick={() =>
               setEditing({
-                id: "",
+                
                 title: "",
                 campus: "regular",
                 description: "",
